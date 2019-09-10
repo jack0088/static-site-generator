2f79b7de1a1755a0588df76e45b83ce65a58f3d0
Alexander Schäfer <herrsch@herrsch-macbook.home>
2019-09-10 21:55:41 +0200

    fixed a lot of shit and made everything more generic to use

--- ssg/plumber.lua
+++ ssg/plumber.lua
@@ -31,29 +31,12 @@ end
 
 
 --[[
-    @path (string) relative or absolute path to folder or file
-    @rights (string or number) octal permission, see http://permissions-calculator.org
-    fs.permission(path) returns (string) an encoded 4 octal digit representing the permission level
-    fs.permission(path, rights) returns (boolean) true for successful assignment at sucessful back-check
---]]
-function fs.permissions(path, rights)
-    local fmt = "%04d"
-    if type(rights) == "number" then
-        -- NOTE I think you can not go below chmod 444 on MacOS because there is always User, Group and Other
-        -- thus trying to set rights to 044 would result in 644
-        return chmod("-R", string.format(fmt, rights), "'"..path.."'").__exitcode == 0 and tonumber(rights) == tonumber(fs.permissions(path))
-    end
-    -- TODO not sure about Linux support, maybe need to check platform and use `stat -c '%a' '<path>'`
-    return string.format(fmt, stat("-r '"..path.."'"):awk("'{print $3}'"):tail("-c 4").__input)
-end
-
-
---[[
     @path (string) relative- or absolute path to the new, empty file
     does not override existing file but updates its timestamp
     returns (boolean) true on success
 --]]
 function fs.makefile(path)
+    if fs.isfolder(path) then return false end
     return touch("'"..path.."'").__exitcode == 0
 end
 
@@ -61,20 +44,12 @@ end
 --[[
     @path (string) relative- or absolute path to the new (sub-)folder
     folder name must not contain special characters, except: spaces, plus- & minus signs and underscores
-    does nothing to existing (sub-)folder
+    does nothing to existing (sub-)folder or its contents
     returns (boolean) true on success
 --]]
 function fs.makefolder(path)
-    if not fs.isfolder(path) then
-        if not fs.isfile(path) then
-            return mkdir("-p", "'"..path.."'").__exitcode == 0
-        else
-            -- path resolves into [existing] file
-            -- or includes special characters
-            return false
-        end
-    end
-    return true -- folder exists already
+    if fs.isfile(path) then return false end
+    return mkdir("-p", "'"..path.."'").__exitcode == 0
 end
 
 
@@ -84,30 +59,48 @@ end
     returns (boolean) true on success
 --]]
 function fs.deletefile(path)
-    if fs.isfile(path) then
-        return rm("-f", "'"..path.."'").__exitcode == 0
-    end
-    return false
+    return rm("-f", "'"..path.."'").__exitcode == 0
 end
 
 
 --[[
     @path (string) relative- or absolute path to the (sub-)folder
+    deletes recursevly any sub-folder and its contents
     skips non-existing folder
     returns (boolean) true on success
 --]]
 function fs.deletefolder(path)
-    if fs.isfolder(path) then
-        return rm("-rf", "'"..path.."'").__exitcode == 0
-    end
-    return false
+    return rm("-rf", "'"..path.."'").__exitcode == 0
 end
 
 
-print(fs.makefolder("foo/baz .-_ lol.txt/bar"))
+--[[
+    @path (string) relative- or absolute path to folder or file
+    @rights (string or number) permission level, see http://permissions-calculator.org
+    fs.permissions(path) returns (string) an encoded 4 octal digit representing the permission level
+    fs.permissions(path, right) recursevly sets permission level and returns (boolean) true for successful assignment
+--]]
+function fs.permissions(path, right)
+    local fmt = "%03d"
+    if type(path) ~= "string" or (not fs.isfile(path) and not fs.isfolder(path)) then return nil end
+    if type(right) == "number" then
+        -- NOTE seems you can not go below chmod 411 on MacOS
+        -- as the operating system resets it automatically to the next higher permission level
+        -- because the User (who created the file) at least holds a read access
+        -- thus trying to set rights to e.g. 044 would result in 644
+        -- which means User group automatically gets full rights (7 bits instead of 0)
+        return chmod("-R", string.format(fmt, right), "'"..path.."'").__exitcode == 0 --and tonumber(fs.permissions(path)) == tonumber(right)
+    end
+    -- TODO not sure about Linux support, maybe need to check platform and use `stat -c '%a' '<path>'`
+    return string.format(fmt, stat("-r '"..path.."'"):awk("'{print $3}'"):tail("-c 4").__input)
+end
 
 
 
+
+print(fs.makefile("blah.txt"))
+print(fs.permissions("blah.txt", 44))
+print(fs.permissions("blah.txt"))
 
 
 
