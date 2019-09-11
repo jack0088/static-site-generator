2fc146dc3a29aadce1f35d6602f06b6c50a09dc4
aschaefer <aschaefer@eurofriwa.de>
2019-09-11 09:39:54 +0200

    changed comment style, added fs.createdat and fs.modifiedat

--- ssg/plumber.lua
+++ ssg/plumber.lua
@@ -3,95 +3,102 @@ require "ssg.sh" -- every os.execute command becomes accessible as global functi
 local fs = {} -- namespace for unix low-level plumbing method wrappers
 
 
---[[
-    @check (string) OS to check against; returns (boolean) true on match
-    platform regex for @check could be: linux*, windows* darwin*, cygwin*, mingw* (everything else might count as unknown)
-    returns (string) operating system identifier
---]]
+-- @str (string): the string to trim
+-- removes whitespaces, tabs and line-break characters from beginning and ending of a string
+-- returns (string)
+function fs.trim(str)
+    if type(str) ~= "string" then str = tostring(str or "") end
+    local mask = "[ \t\r\n]*"
+    local result = str:gsub("^"..mask, ""):gsub(mask.."$", "")
+    return result
+end
+
+
+-- @check (string) OS to check against; returns (boolean) true on match
+-- platform regex for @check could be: linux*, windows* darwin*, cygwin*, mingw* (everything else might count as unknown)
+-- returns (string) operating system identifier
 function fs.platform(check)
-    local plat = uname("-s").__input
+    local plat = fs.trim(uname("-s").__input)
     if type(check) == "string" then return type(string.match(string.lower(plat), "^"..string.lower(check))) ~= nil end
     return plat
 end
 
 
---[[
-    @path (string) relative- or absolute path to a file or folder
-    returns (boolean)
---]]
+-- @path (string) relative- or absolute path to a file or folder
+-- returns (boolean)
 function fs.exists(path)
     return test("-e", "'"..path.."'").__exitcode == 0
 end
 
 
---[[
-    @path (string) relative- or absolute path to a file
-    returns (boolean)
---]]
+-- @path (string) relative- or absolute path to a file
+-- returns (boolean)
 function fs.isfile(path)
     return test("-f", "'"..path.."'").__exitcode == 0
 end
 
 
---[[
-    @path (string) relative- or absolute path to a folder
-    returns (boolean)
---]]
+-- @path (string) relative- or absolute path to a folder
+-- returns (boolean)
 function fs.isfolder(path)
     return test("-d", "'"..path.."'").__exitcode == 0
 end
 
 
---[[
-    @path (string) relative- or absolute path to the new, empty file
-    does not override existing file but updates its timestamp
-    returns (boolean) true on success
---]]
+-- @path (string) relative- or absolute path to the new, empty file
+-- does not override existing file but updates its timestamp
+-- returns (boolean) true on success
 function fs.makefile(path)
     if fs.isfolder(path) then return false end
     return touch("'"..path.."'").__exitcode == 0
 end
 
 
---[[
-    @path (string) relative- or absolute path to the new (sub-)folder
-    folder name must not contain special characters, except: spaces, plus- & minus signs and underscores
-    does nothing to existing (sub-)folder or its contents
-    returns (boolean) true on success
---]]
+-- @path (string) relative- or absolute path to the new (sub-)folder
+-- folder name must not contain special characters, except: spaces, plus- & minus signs and underscores
+-- does nothing to existing (sub-)folder or its contents
+-- returns (boolean) true on success
 function fs.makefolder(path)
     if fs.isfile(path) then return false end
     return mkdir("-p", "'"..path.."'").__exitcode == 0
 end
 
 
---[[
-    @path (string) relative- or absolute path to the file
-    skips non-existing file as well
-    returns (boolean) true on success
---]]
+-- @path (string) relative- or absolute path to the file
+-- skips non-existing file as well
+-- returns (boolean) true on success
 function fs.deletefile(path)
     return rm("-f", "'"..path.."'").__exitcode == 0
 end
 
 
---[[
-    @path (string) relative- or absolute path to the (sub-)folder
-    deletes recursevly any sub-folder and its contents
-    skips non-existing folder
-    returns (boolean) true on success
---]]
+-- @path (string) relative- or absolute path to the (sub-)folder
+-- deletes recursevly any sub-folder and its contents
+-- skips non-existing folder
+-- returns (boolean) true on success
 function fs.deletefolder(path)
     return rm("-rf", "'"..path.."'").__exitcode == 0
 end
 
 
---[[
-    @path (string) relative- or absolute path to folder or file
-    @rights (string or number) permission level, see http://permissions-calculator.org
-    fs.permissions(path) returns (string) an encoded 4 octal digit representing the permission level
-    fs.permissions(path, right) recursevly sets permission level and returns (boolean) true for successful assignment
---]]
+-- @path (string) relative- or absolute path to the file or (sub-)folder
+-- returns (string) epoch/ unix date timestamp
+function fs.createdat(path)
+    return fs.trim(stat("-f", "%B", "'"..path.."'").__input) -- TODO check on other platforms than MacOS
+end
+
+
+-- @path (string) relative- or absolute path to the file or (sub-)folder
+-- returns (string) epoch/ unix date timestamp
+function fs.modifiedat(path)
+    return fs.trim(date("-r", "'"..path.."'", "+%s").__input)
+end
+
+
+-- @path (string) relative- or absolute path to folder or file
+-- @rights (string or number) permission level, see http://permissions-calculator.org
+-- fs.permissions(path) returns (string) an encoded 4 octal digit representing the permission level
+-- fs.permissions(path, right) recursevly sets permission level and returns (boolean) true for successful assignment
 function fs.permissions(path, right)
     local fmt = "%03d"
     if type(path) ~= "string" or (not fs.isfile(path) and not fs.isfolder(path)) then return nil end
@@ -104,7 +111,7 @@ function fs.permissions(path, right)
         return chmod("-R", string.format(fmt, right), "'"..path.."'").__exitcode == 0 --and tonumber(fs.permissions(path)) == tonumber(right)
     end
     if fs.platform("darwin") then -- MacOS
-        return string.format(fmt, stat("-r '"..path.."'"):awk("'{print $3}'"):tail("-c 4").__input)
+        return string.format(fmt, fs.trim(stat("-r '"..path.."'"):awk("'{print $3}'"):tail("-c 4").__input))
     elseif fs.platform("linux") then -- Linux
         return stat("-c", "'%a'", "'"..path.."'") -- TODO needs testing
     elseif fs.platform("windows") then -- Windows
