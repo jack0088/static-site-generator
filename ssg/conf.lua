12b6fb8632fe717a90a6cc4ea7d7c625e8ec23a4
aschaefer <aschaefer@eurofriwa.de>
2019-09-16 10:49:23 +0200

    inserted all possible conf keys

--- ssg/conf.lua
+++ ssg/conf.lua
@@ -6,8 +6,8 @@ function love.conf(t)
     t.modules.audio = false
     t.modules.video = false
     t.modules.joystick = false
-    t.modules.keyboard = false
-    t.modules.mouse = false
+    t.modules.keyboard = true
+    t.modules.mouse = true
     t.modules.touch = false
     t.modules.data = true
     t.modules.math = true
@@ -16,5 +16,34 @@ function love.conf(t)
     t.modules.thread = true
     t.modules.event = true
     t.modules.timer = true
-    t.modules.window = true -- false for headless build
+    t.modules.window = true -- false for a headless build
+ 
+    t.window.title = "ssg"
+    t.window.icon = nil
+    t.window.x = nil
+    t.window.y = nil
+    t.window.width = 400
+    t.window.height = 200
+    t.window.minwidth = 400
+    t.window.minheight = 100
+    t.window.borderless = false
+    t.window.resizable = true
+    t.window.fullscreen = false
+    t.window.fullscreentype = "desktop"
+    t.window.vsync = 1
+    t.window.msaa = 0
+    t.window.depth = nil
+    t.window.stencil = nil
+    t.window.highdpi = false
+    t.window.display = 1
+
+    t.audio.mixwithsystem = false -- iOS, Android
+    
+    t.version = "11.0"
+    t.identity = "ssg"
+    t.appendidentity = false
+    t.console = false -- Windows
+    t.accelerometerjoystick = false
+    t.externalstorage = true -- Android
+    t.gammacorrect = false
 end
