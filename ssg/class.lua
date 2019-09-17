6fbe777abf7ef0a6f657d4fc85979f438174a395
Alexander Schäfer <herrsch@herrsch-macbook.home>
2019-09-17 21:32:54 +0200

    rewrote class lib to support getters and setters (still testing it)

--- ssg/class.lua
+++ ssg/class.lua
@@ -12,40 +12,65 @@
 -- This is the super (base) class for all new class objects
 -- which has important methods that all classes might want to use
 -- This super class is NOT copied to instance objects once classes get instanciated!
-local super = {
-    -- Return the parent object of a given class object
-    -- @something (required table): class object whose parent should be returned
-    parent = function(something)
-        return (getmetatable(something) or {}).__index
-    end;
-    -- Check if an object is a parent of nother one
-    -- @anything (required table): reference object whose relation we want to check (the derivant)
-    -- @something (required table): relative object which might be the parent of our derivant (=@anything)
-    derivant = function(anything, something)
-        return anything:parent() == something
-    end
-}
+local super = {}
 super.__index = super
+
+-- Return the parent object of a given class object
+-- @klass (required table): class object whose parent should be returned
+super.parent = function(klass) return klass.__parent end
+
+-- Check if an object is a parent of nother one
+-- @klass (required table): reference object whose relation we want to check (the derivant)
+-- @subklass (required table): relative object which might be the parent of our derivant (=@klass)
+super.derivant = function(klass, subklass) return klass:parent() == subklass end
 
 -- Make a shallow copy of a class while walking down the entire inheritance chain
 -- Call the constructor of the new class instance (if there is any) and return its return value
 -- or simply return the new class instance itself, if there the instance has no constructor method
--- @something (required table): class object to shallow-copy recursevly
+-- @array (required table): class object to shallow-copy recursevly
 -- @... (optional arguments): argements are passed to the optional class constructor
-local function replica(something, ...)
-    local meta = getmetatable(something)
-    local copy = meta and replica(meta.__index) or {}
-    if something ~= super then for k, v in pairs(something) do copy[k] = v end end
+local function replica(array, ...)
+    local copy = array.__parent and replica(array.__parent) or {}
+    if array ~= super then for k, v in pairs(array) do copy[k] = v end end
     return copy.new and (copy:new(...) or copy) or copy
 end
 
 -- Create a new class object or create a sub-class from an already existing class
 -- @something (optional table): parent class to sub-call from
-local function thing(something)
-    if not something or not getmetatable(something) then something = setmetatable(something or {}, super) end
-    return setmetatable({}, {__index = something, __call = replica})
+local function class(parent)
+    local proxy = function(array, key, value)
+        if type(array) == "nil" or type(key) == "nil" then return nil end
+        if type(value) == "nil" then
+            if key:lower():match("^[gs]et_(.+)") then return rawget(array, key) end -- access with getter/setter prefix
+            local getter = rawget(array, "get_"..key) -- try find getter on prefixless access
+            if type(getter) == "function" then return getter(array) end -- however ignore non-function getter
+            return rawget(array, key) or array.__parent[key]
+        end
+        if key:lower():match("^[gs]et_(.+)") then -- with getter/setter prefix
+            assert(type(value) == "function", "getter/setter assignment must be a function value")
+            rawset(array, key, value)
+            return value
+        end
+        local setter = rawget(array, "set_"..key)
+        if type(setter) == "function" then return setter(array, value) or value end
+        rawset(array, key, value)
+        return value
+    end
+    return setmetatable({__parent = parent or super}, {__index = proxy, __newindex = proxy, __call = replica})
 end
 
--- TODO somehow implement getters & setters on top of this library
 
-return thing
+local pretty = require "prettify"
+local thing = class()
+thing.foobar = "foobarval"
+function thing:get_lol() return self.__lol__value__ end
+function thing:set_lol(v) self.__lol__value__ = v return "fuuuck yeas!" end
+thing.lol = "newlolvalthroughsetter"
+
+local men = class(thing)
+men.human = true
+men.lol = "menlol"
+print(pretty(men.lol), thing.lol)
+
+
+return class
