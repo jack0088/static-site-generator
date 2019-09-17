dd47dc248a767b070687deec11ee9f881331f8bd
Alexander Schäfer <herrsch@herrsch-macbook.home>
2019-09-17 21:34:52 +0200

    just testing different things with filedropped and folderdropped

--- ssg/main.lua
+++ ssg/main.lua
@@ -8,7 +8,7 @@ end
 local pretty = require "prettify"
 local mime = require "mimetype"
 local json = require "json"
-local fs = require "disk"
+local fs = require "floppy"
 
 
 function love.load()
@@ -21,12 +21,14 @@ function love.draw()
     love.graphics.setBackgroundColor(.2, .2, .3, 1)
 end
 
-
--- TODO evaluate fallowing options for loading projects
-
 --love.system.openURL("file://") -- for running the project?
 
 function love.directorydropped(path)
     print("dropped folder:", path)
     --fs.writefile(path.."/config.json", "huhu, hello world!")
+end
+
+
+function love.filedropped(data)
+    print(data)
 end
