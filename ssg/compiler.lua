c73a19121640a081f73514e58044e2caafdf9031
aschaefer <aschaefer@eurofriwa.de>
2019-09-26 15:46:57 +0200

    renamed lib to compiler to stay in the naming convention

--- ssg/compiler.lua
+++ ssg/compiler.lua
@@ -1,12 +1,12 @@
 -- Trace placeholders inside files and repace them by other file contents
 -- 2019 (c) kontakt@herrsch.de
 
-local pretty = require "prettify"
 local fs = require "floppy"
 local tracer = {}
 
 
 function tracer.render(folder)
+    print(type(folder), folder)
     print(fs.exists(folder), fs.isfolder(folder), fs.isfile(folder))
     -- if not fs.isfile(folder.."/index.html") then print("oh") return false end
     -- print(folder)
